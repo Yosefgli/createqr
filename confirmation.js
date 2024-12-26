7bcde30df5d1553ea2e2bdc7a2f3db1f1cefa532
Yosefgli <ygli770@gmail.com>
2024-12-27 00:43:37 +0500

    Delete confirmation.js

--- confirmation.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const shabbatDate = urlParams.get('date');
-    const eveningCount = urlParams.get('evening');
-    const morningCount = urlParams.get('morning');
-
-    const detailsElement = document.getElementById('registration-details');
-    detailsElement.textContent = `נרשמת בהצלחה לשבת ${shabbatDate} עם ${eveningCount} אנשים בערב ו-${morningCount} אנשים בבוקר.`;
-});
